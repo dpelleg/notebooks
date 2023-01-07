b60b63a876c7a706e251dce0230e264312b67a33
Dan Pelleg <dan-envmon@pelleg.org>
2023-01-07 18:28:33 +0200

    pythonized the daily script

---
+++ mudguard/daily.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import subprocess
+import conf
+
+# change dir to the script's dir
+os.chdir(sys.path[0])
+
+subprocess.run(["python3", "./download_weather.py"])
+subprocess.run(["python3", "./get_all_segments.py"])
+subprocess.run(["python3", "./genrides.py"], check=True)
+srcfile = "{}out/rides.html".format(conf.conf['datadir'])
+subproces.run("mv {} ../../mysite/static/rides.html".format(srcfile))
